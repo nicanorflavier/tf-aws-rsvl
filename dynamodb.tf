26fffe41e3c64e1a734ab654945755e44a8e3ed8
nicanorflavier <nicanorflavier@outlook.com>
2024-04-26 05:37:17 +1000

    adjusted for readability

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,12 +1,12 @@
 #Define the DynamoDB table for Terraform state locking
 resource "aws_dynamodb_table" "dynamodb_tf_locks" {
-  name = "${var.environment_name}-${var.dynamodb_table_name}"
-  billing_mode = "PROVISIONED"
-  read_capacity  = 5 #Change to your value as per your reference
-  write_capacity = 5 #Change to your value as per your reference
-  hash_key     = "LockID"
+  name                        = "${var.environment_name}-${var.dynamodb_table_name}"
+  billing_mode                = "PROVISIONED"
+  read_capacity               = 5 #Change to your value as per your reference
+  write_capacity              = 5 #Change to your value as per your reference
+  hash_key                    = "LockID"
   deletion_protection_enabled = false #Change to true when you want to prevent the destruction of the DynamoDB table
-  
+
   attribute {
     name = "LockID"
     type = "S"
@@ -21,7 +21,7 @@ resource "aws_dynamodb_table" "dynamodb_tf_locks" {
   }
 
   tags = {
-	  Name = var.dynamodb_table_name
+    Name = var.dynamodb_table_name
   }
 
 }
